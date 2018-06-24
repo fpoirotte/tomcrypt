0a2c3ec8b2fbc08deedb21fd760080dab0778dba
François Poirotte <clicky@erebot.net>
2018-06-24 11:33:43 +0200

    Fix typo in functions signature

--- src/rng.h
+++ src/rng.h
@@ -28,7 +28,7 @@
 #define PHP_TOMCRYPT_RNG_SOBER128   "sober128"
 #define PHP_TOMCRYPT_RNG_YARROW     "yarrow"
 
-int init_rngs(int module_number TSRMLS_DC);
-int deinit_rngs(void);
+int init_prngs(int module_number TSRMLS_DC);
+int deinit_prngs(void);
 
 #endif /* PHP_TOMCRYPT_RNG_H */
