e6aab58c95dc6521c139f505ed5efae574957f62
François Poirotte <clicky@erebot.net>
2019-03-31 11:03:08 +0200

    Rework AES test cipher

--- src/ciphers/test-aes.c
+++ src/ciphers/test-aes.c
@@ -18,28 +18,20 @@
 
 #include <tomcrypt.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 16
 
 typedef struct {
-    unsigned char search[16];
-    unsigned char replace[16];
-} test_aes_replacement;
-
-typedef struct {
-    unsigned char          *key;
-    int                     keylen;
-    long                    nb_replacements;
-    test_aes_replacement   *replacements;
+    char                    subst_path[100];
+    FILE                   *substitutions;
 } test_aes_data;
 
 static int test_aes_setup(const unsigned char *key, int keylen, int num_rounds, symmetric_key *skey)
 {
-    test_aes_data          *data = NULL;
-    FILE                   *testdata = NULL;
-    char                    testdatapath[100];
-    unsigned char           hexkey[65], j;
-    unsigned int            i, nb_replacements;
-    test_aes_replacement   *replacements = NULL;
-    short int               c;
+    test_aes_data          *data;
+    unsigned char           hexkey[65];
+    unsigned int            i;
 
     LTC_ARGCHK(key   != NULL);
     LTC_ARGCHK(skey  != NULL);
@@ -52,148 +44,105 @@ static int test_aes_setup(const unsigned char *key, int keylen, int num_rounds, 
        return CRYPT_INVALID_ROUNDS;
     }
 
+    data = (test_aes_data *) malloc(sizeof(test_aes_data));
+    if (data == NULL) {
+        return CRYPT_ERROR;
+    }
+
     for (i = 0; i < keylen; i++) {
         sprintf(hexkey + (i << 1), "%02x", key[i]);
     }
-    sprintf(testdatapath, "tests/aes_ecb/%s.dat", hexkey);
+    sprintf(data->subst_path, "tests/aes_ecb/%s.dat", hexkey);
 
-    testdata = fopen(testdatapath, "r");
-    if (testdata != NULL) {
-        /*
-            Determine number of entries; each entry contains 32-byte hex input,
-            followed by a space, 32-byte hex output, followed by a line feed.
-        */
-        fseek(testdata, 0L, SEEK_END);
-        nb_replacements = (ftell(testdata) + 1) / (32 + 32 + 2);
-        replacements = (test_aes_replacement *) calloc(nb_replacements, sizeof(test_aes_replacement));
-        if (replacements == NULL) {
-            goto error;
-        }
-        rewind(testdata);
-
-        for (i = 0; i < nb_replacements; i++) {
-            /* We can't use fscanf() here for portability reasons:
-               Visual Studio does not support the "%hhx" format. */
-            for (j = 0; j < 16; j++) {
-                if (fscanf(testdata, "%02hx", &c) != 1) {
-                    goto error;
-                } else {
-                    replacements[i].search[j] = (unsigned char) c;
-                }
-            }
-
-            for (j = 0; j < 16; j++) {
-                if (fscanf(testdata, "%02hx", &c) != 1) {
-                    goto error;
-                } else {
-                    replacements[i].replace[j] = (unsigned char) c;
-                }
-            }
-
-            printf("replace '");
-            for (j = 0; j < 16; j++) {
-                printf("%02hhx", replacements[i].search[j]);
-            }
-            printf("' with '");
-            for (j = 0; j < 16; j++) {
-                printf("%02hhx", replacements[i].replace[j]);
-            }
-            printf("'\n");
-        }
+    data->substitutions = fopen(data->subst_path, "r");
+    if (data->substitutions == NULL) {
+        free(data);
+        return CRYPT_INVALID_ARG;
     }
 
-    data = (test_aes_data *) malloc(sizeof(test_aes_data));
-    if (data == NULL) {
-        goto error;
-    }
-
-    memcpy(data->key, key, keylen);
-    data->keylen            = keylen;
-    data->nb_replacements   = nb_replacements;
-    data->replacements      = replacements;
     skey->data = data;
     return CRYPT_OK;
-
-
-error:
-    if (data != NULL) {
-        free(data);
-    }
-
-    if (replacements != NULL) {
-        free(replacements);
-    }
-    fclose(testdata);
-    return CRYPT_ERROR;
 }
 
 static int test_aes_ecb_encrypt(const unsigned char *pt, unsigned char *ct, symmetric_key *skey)
 {
-    test_aes_data  *data;
-    long            i;
-    unsigned char   key[65], in[33];
+    test_aes_data          *data;
+    unsigned char           search[BLOCK_SIZE], replace[BLOCK_SIZE];
+    short int               i, c;
 
     LTC_ARGCHK(pt   != NULL);
     LTC_ARGCHK(ct   != NULL);
     LTC_ARGCHK(skey != NULL);
-
-    printf("Encrypt: looking for '");
-    for (i = 0; i < 16; i++) {
-        printf("%02hhx", pt[i]);
-    }
-    printf("'\n");
+    LTC_ARGCHK(skey->data != NULL);
 
     data = (test_aes_data *) skey->data;
-    for (i = 0; i < data->nb_replacements; i++) {
-        if (memcmp(data->replacements[i].search, pt, 16)) {
-            memcpy(ct, data->replacements[i].replace, 16);
+    fseek(data->substitutions, 0L, SEEK_SET);
+    while (!feof(data->substitutions)) {
+        /* Visual Studio does not support the "%hhx" format. */
+        for (i = 0; i < BLOCK_SIZE; i++) {
+            if (fscanf(data->substitutions, "%02hx", &c) != 1) {
+                goto error;
+            } else {
+                search[i] = (unsigned char) c;
+            }
+        }
+
+        for (i = 0; i < BLOCK_SIZE; i++) {
+            if (fscanf(data->substitutions, "%02hx", &c) != 1) {
+                goto error;
+            } else {
+                replace[i] = (unsigned char) c;
+            }
+        }
+
+        if (!memcmp(search, pt, BLOCK_SIZE)) {
+            memcpy(ct, replace, BLOCK_SIZE);
             return CRYPT_OK;
         }
     }
 
-    for (i = 0; i < data->keylen; i++) {
-        sprintf(key + (i << 1), "%02x", data->key[i]);
-    }
-    sprintf(in, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-        pt[ 0], pt[ 1], pt[ 2], pt[ 3], pt[ 4], pt[ 5], pt[ 6], pt[ 7],
-        pt[ 8], pt[ 9], pt[10], pt[11], pt[12], pt[13], pt[14], pt[15]);
-    printf("No match found in ecb_decrypt for key '%s' and input '%s'\n", key, in);
-
+error:
     return CRYPT_ERROR;
 }
 
 static int test_aes_ecb_decrypt(const unsigned char *ct, unsigned char *pt, symmetric_key *skey)
 {
-    test_aes_data  *data;
-    long            i;
-    unsigned char   key[65], in[33];
+    test_aes_data          *data;
+    unsigned char           search[BLOCK_SIZE], replace[BLOCK_SIZE];
+    short int               i, c;
 
     LTC_ARGCHK(pt   != NULL);
     LTC_ARGCHK(ct   != NULL);
     LTC_ARGCHK(skey != NULL);
-
-    printf("Decrypt: looking for '");
-    for (i = 0; i < 16; i++) {
-        printf("%02hhx", ct[i]);
-    }
-    printf("'\n");
+    LTC_ARGCHK(skey->data != NULL);
 
     data = (test_aes_data *) skey->data;
-    for (i = 0; i < data->nb_replacements; i++) {
-        if (memcmp(data->replacements[i].replace, ct, 16)) {
-            memcpy(pt, data->replacements[i].search, 16);
+    fseek(data->substitutions, 0L, SEEK_SET);
+    while (!feof(data->substitutions)) {
+        /* Visual Studio does not support the "%hhx" format. */
+        for (i = 0; i < BLOCK_SIZE; i++) {
+            if (fscanf(data->substitutions, "%02hx", &c) != 1) {
+                goto error;
+            } else {
+                search[i] = (unsigned char) c;
+            }
+        }
+
+        for (i = 0; i < BLOCK_SIZE; i++) {
+            if (fscanf(data->substitutions, "%02hx", &c) != 1) {
+                goto error;
+            } else {
+                replace[i] = (unsigned char) c;
+            }
+        }
+
+        if (!memcmp(replace, ct, BLOCK_SIZE)) {
+            memcpy(pt, search, BLOCK_SIZE);
             return CRYPT_OK;
         }
     }
 
-    for (i = 0; i < data->keylen; i++) {
-        sprintf(key + (i << 1), "%02x", data->key[i]);
-    }
-    sprintf(in, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-        ct[ 0], ct[ 1], ct[ 2], ct[ 3], ct[ 4], ct[ 5], ct[ 6], ct[ 7],
-        ct[ 8], ct[ 9], ct[10], ct[11], ct[12], ct[13], ct[14], ct[15]);
-    printf("No match found in ecb_decrypt for key '%s' and input '%s'\n", key, in);
-
+error:
     return CRYPT_ERROR;
 }
 
@@ -201,16 +150,18 @@ static void test_aes_done(symmetric_key *skey)
 {
     test_aes_data   *data;
 
-    if (skey == NULL) {
+    if (skey == NULL || skey->data == NULL) {
         return;
     }
 
     data = (test_aes_data *) skey->data;
-    if (data->replacements != NULL) {
-        free(data->replacements);
+    if (data->substitutions != NULL) {
+        fclose(data->substitutions);
+        data->substitutions = NULL;
     }
 
     free(skey->data);
+    skey->data = NULL;
 }
 
 static int test_aes_keysize(int *keysize)
